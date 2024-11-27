8e2fe34212dda0b1df8ea82132ac080da30616a4
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-27 08:54:07 +0630

    ads banner updated

--- src/components/AdsBanner.jsx
+++ src/components/AdsBanner.jsx
@@ -1,32 +1,33 @@
-
-import React, { useEffect } from 'react'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-import useFetch from '../hooks/useFetch'
-import BASE_URL from '../hooks/baseUrl'
+import React, { useEffect } from 'react';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import useFetch from '../hooks/useFetch';
+import BASE_URL from '../hooks/baseUrl';
 
 const AdsBanner = () => {
-    const MySwal = withReactContent(Swal)
-    const {data: popup} = useFetch(BASE_URL + "/ads-banner");
-    // console.log(popup);
-    
-    const adsFire=()=>{
-        MySwal.fire({
-             imageUrl: popup?.img_url,
-            imageHeight:150,
-             text:"🙏🏻မင်္ဂလာရှိသော နေ့ခင်းလေးပါ👦🏻 သူငယ်ချင်းမိတ်ဆက် 10%💰 🏠အိမ်မှာနေရင်း 🎰 Golden Jack မှာပူးပေါင်းပြီး🤝 အလွယ်တကူ ဝင်ငွေရှာလိုက်ပါ သူငယ်ချင်းမိတ်ဆက်ပေးပြီး ဘယ်မှာမှ မရနှိင်တဲ့ 🏆ဆုလက်ဆောင် 10% များရယူပါနော်  Golden Jack 🙏🙏🙏🤝🙏🙏🙏"
-           })
-    }
-    useEffect(()=>{
-      if(popup.img_url){
-        adsFire()
-      }
-    },[popup.img_url]);
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const MySwal = withReactContent(Swal);
+  const { data: popup } = useFetch(BASE_URL + '/ads-banner');
 
-export default AdsBanner
+  // Function to display the banner popup
+  const adsFire = () => {
+    if (popup && popup.img_url) {
+      MySwal.fire({
+        imageUrl: popup.img_url,
+        imageHeight: 150,
+        text: '🙏🏻မင်္ဂလာရှိသော နေ့ခင်းလေးပါ👦🏻 သူငယ်ချင်းမိတ်ဆက် 10%💰 🏠အိမ်မှာနေရင်း 🎰 AMK Max Win 999  မှာပူးပေါင်းပြီး🤝 အလွယ်တကူ ဝင်ငွေရှာလိုက်ပါ သူငယ်ချင်းမိတ်ဆက်ပေးပြီး ဘယ်မှာမှ မရနှိင်တဲ့ 🏆ဆုလက်ဆောင် 10% များရယူပါနော်  AMK Max Win 999 🙏🙏🙏🤝🙏🙏🙏',
+      });
+    } else {
+      console.warn('Popup data or img_url is not available');
+    }
+  };
+
+  useEffect(() => {
+    if (popup?.img_url) {
+      adsFire();
+    }
+  }, [popup?.img_url]);
+
+  return <div></div>;
+};
+
+export default AdsBanner;
